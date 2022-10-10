556a50b5a57a53a82d170c3d868d6a8024a1f573
Dennis Gaida <2392217+DennisGaida@users.noreply.github.com>
2022-10-10 10:21:01 +0200

    Changed web scraping to offical API request
    Inspiration taken from https://github.com/ionred/chocolatey-packages

--- microsoft-edge/update.ps1
+++ microsoft-edge/update.ps1
@@ -1,10 +1,6 @@
 Import-Module au
-$download_scrape_url     = "https://www.microsoft.com/en-us/edge/business/download"
-$download_scrape_version = "Stable"
-
-$artifact_base_url       = "https://www.microsoft.com/en-us/edge/business/Product/GetArtifacturl"
-$artifact_arch32         = "x86"
-$artifact_arch64         = "x64"
+$download_api_url     = "https://edgeupdates.microsoft.com/api/products"
+$release_branch       = "Stable"
 
 function global:au_SearchReplace {
   @{
@@ -18,25 +14,22 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-  # get contents of the download page (HTML)
-  $download_scrape_content = Invoke-WebRequest -Uri $download_scrape_url
-  
+  # get contents of the Edge download API response
+  $download_api_content = (Invoke-WebRequest -Uri $download_api_url).Content
+    
   # grab the JSON containing all version/download information
-  $json_node = ($download_scrape_content.AllElements | ? {$_.id -eq 'commercial-json-data'} | select -first 1)
-  $json_data = $json_node.'data-whole-json' | ConvertFrom-Json
+  $json_data = $download_api_content | ConvertFrom-Json
   
   # select the release version (Dev/Beta/Stable)
-  $download_url_data = $json_data | ? {$_.Product -eq $download_scrape_version}
+  $releases = $json_data | Where-Object -Property Product -eq $release_branch | Select-Object Releases
   
-  # select the first version per architecture from the JSON - this should be the latest version
-  $download_32_data = $download_url_data.Releases | ? {$_.Architecture -eq $artifact_arch32 -and $_.Platform -eq "Windows"} | select -first 1
-  $download_64_data = $download_url_data.Releases | ? {$_.Architecture -eq $artifact_arch64 -and $_.Platform -eq "Windows"} | select -first 1
-  $download_url_32 = $download_32_data.Artifacts[0].Location
-  $download_hash_32 = $download_32_data.Artifacts[0].Hash
-  $download_url_64 = $download_64_data.Artifacts[0].Location
-  $download_hash_64 = $download_64_data.Artifacts[0].Hash
-    
-  $version_number = $download_32_data.ProductVersion.Trim()
+  # get download URLs and hashes from JSON
+  $download_url_32 = ($releases.Releases | Where-Object {$_.Platform -eq 'Windows'} | Where-Object {$_.Architecture -eq 'x86'}).Artifacts.Location
+  $download_hash_32 = ($releases.Releases | Where-Object {$_.Platform -eq 'Windows'} | Where-Object {$_.Architecture -eq 'x86'}).Artifacts.Hash
+  $download_url_64 = ($releases.Releases | Where-Object {$_.Platform -eq 'Windows'} | Where-Object {$_.Architecture -eq 'x64'}).Artifacts.Location
+  $download_hash_64 = ($releases.Releases | Where-Object {$_.Platform -eq 'Windows'} | Where-Object {$_.Architecture -eq 'x64'}).Artifacts.Hash
+
+  $version_number = ($releases.Releases | Where-Object {$_.Platform -eq 'Windows'} | Where-Object {$_.Architecture -eq 'x64'}).ProductVersion.Trim()
   
   $Latest = @{URL32 = $download_url_32; URL64 = $download_url_64; Version = $version_number; Checksum32 = $download_hash_32; Checksum64 = $download_hash_64;}
   return $Latest

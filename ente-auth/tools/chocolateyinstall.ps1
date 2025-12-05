1258428db79fc2390ade363bfff4cc4a0d999096
DennisGaida <2392217+DennisGaida@users.noreply.github.com>
2025-12-05 10:27:53 +0000

    AU: 1 updated - ente-auth [skip ci] https://gist.github.com/DennisGaida/fd90446667b0deac773d9206c6981be1/d54ad9fe4e368aaeeaa2fc5bdd15db23ca913702

--- ente-auth/tools/chocolateyinstall.ps1
+++ ente-auth/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿
 $ErrorActionPreference = 'Stop';
-$url32 = 'https://github.com/ente-io/ente/releases/download/auth-v4.4.10/ente-auth-v4.4.10-installer.exe'
+$url32 = 'https://github.com/ente-io/ente/releases/download/auth-v4.4.12/ente-auth-v4.4.12-installer.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
@@ -9,7 +9,7 @@ $packageArgs = @{
 
   softwareName   = 'Ente Auth'
 
-  checksum       = '81e9208e113e7fea2cf269209e8be14c91097b6b984760fb43bcedd116bcb626'
+  checksum       = '25ede4349c36644c7b2cbdf3fa5a47646050c78bcfabee5600f17a4dd94f79be'
   checksumType   = 'sha256'
 
   silentArgs     = "{0} /VERYSILENT /SUPPRESSMSGBOXES /LOG=`"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).installer.log`""
